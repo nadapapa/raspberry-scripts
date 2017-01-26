9dc80eddfacda4aa20727cca10728c8c7abe3c89
nadapapa <nadapapa@gmail.com>
2017-01-26 14:39:32 +0100

    fix

--- motion.py
+++ motion.py
@@ -13,6 +13,11 @@ camera.hflip = True
 GPIO.setmode(GPIO.BCM)
 GPIO.setwarnings(False)
 GPIO.setup(17, GPIO.OUT)
+GPIO.output(17, 1)
+# test led
+#GPIO.setup(27, GPIO.OUT)
+#GPIO.output(27, 1)
+
 
 print("start")
 
@@ -21,13 +26,13 @@ try:
         time.sleep(0.5)
         pir.wait_for_motion()
         print("motion")
-        GPIO.output(17, 1)
+        GPIO.output(17, 0)
         filename = datetime.now().strftime("%Y-%m-%d_%H.%M.%S.h264")    
         camera.start_recording("/home/pi/videos/" + filename)
            
         pir.wait_for_no_motion()
         print("no motion")
-        GPIO.output(17, 0)
+        GPIO.output(17, 1)
         camera.stop_recording()
 
 except KeyboardInterrupt:          # trap a CTRL+C keyboard interrupt  
