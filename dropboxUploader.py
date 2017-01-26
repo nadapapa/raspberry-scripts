0a3e72125aa361933dd73fcaec2fbc17652bf66d
nadapapa <nadapapa@gmail.com>
2017-01-26 17:08:52 +0100

    refactor

---
+++ dropboxUploader.py
@@ -0,0 +1,57 @@
+import dropbox
+import os
+import configparser
+
+config = configparser.ConfigParser()
+config.read("config.ini")
+
+app_key = config["DROPBOX"]["app_key"]
+app_secret = config["DROPBOX"]["app_secret"]
+
+
+def dropboxAuth():
+    accessTokenFileOverwrite = open("accessToken.txt", "w+")
+
+    flow = dropbox.client.DropboxOAuth2FlowNoRedirect(app_key, app_secret)
+
+    # Have the user sign in and authorize this token
+    authorize_url = flow.start()
+    print('1. Go to: ' + authorize_url)
+    print('2. Click "Allow" (you might have to log in first)')
+    print('3. Copy the authorization code.')
+    code = input("Enter the authorization code here: ").strip()
+
+    try:
+        # This will fail if the user enters an invalid authorization code
+        access_token, user_id = flow.finish(code)
+        accessTokenFileOverwrite.write(access_token)
+    except:
+        print("failed authorization, restart")
+        accessTokenFileOverwrite.close()
+        os.remove("accessToken.txt")
+
+    accessTokenFileOverwrite.close()
+
+
+def dropboxUpload(fileToUpload):
+    if not os.path.isfile("accessToken.txt"):
+        dropboxAuth()
+
+    # get access token from file
+    accessTokenFileRead = open("accessToken.txt", "r")
+    access_token = accessTokenFileRead.read().rstrip()
+    accessTokenFileRead.close()
+
+    # make client
+    client = dropbox.client.DropboxClient(access_token)
+
+    # upload file
+    fileToUploadObject = open(fileToUpload, "rb")
+    response = client.put_file(fileToUpload, fileToUploadObject)
+    fileToUploadObject.close()
+
+    return 1
+
+if __name__ == "__main__":
+    import sys
+    sys.exit(dropboxUpload(sys.argv[0]))
--- motion.py
+++ motion.py
@@ -2,16 +2,10 @@ from gpiozero import MotionSensor
 from picamera import PiCamera
 from datetime import datetime
 import RPi.GPIO as GPIO
-import dropbox
 import time
-import os
-import configparser
+import sys
+import subprocess
 
-config = configparser.ConfigParser()
-config.read("config.ini")
-
-app_key = config["DROPBOX"]["app_key"]
-app_secret = config["DROPBOX"]["app_secret"]
 relay_pin = 17
 
 GPIO.setmode(GPIO.BCM)
@@ -28,48 +22,6 @@ GPIO.output(relay_pin, 1)
 
 print("start")
 
-def dropboxAuth():
-    accessTokenFileOverwrite = open("accessToken.txt", "w+")
-
-    flow = dropbox.client.DropboxOAuth2FlowNoRedirect(app_key, app_secret)
-
-    # Have the user sign in and authorize this token
-    authorize_url = flow.start()
-    print('1. Go to: ' + authorize_url)
-    print('2. Click "Allow" (you might have to log in first)')
-    print('3. Copy the authorization code.')
-    code = input("Enter the authorization code here: ").strip()
-
-    try:
-        # This will fail if the user enters an invalid authorization code
-        access_token, user_id = flow.finish(code)
-        accessTokenFileOverwrite.write(access_token)
-    except:
-        print("failed authorization, restart")
-        accessTokenFileOverwrite.close()
-        os.remove("accessToken.txt")
-
-    accessTokenFileOverwrite.close()
-
-
-def dropboxUpload(fileToUpload):
-    if not os.path.isfile("accessToken.txt"):
-        dropboxAuth()
-
-    # get access token from file
-    accessTokenFileRead = open("accessToken.txt", "r")
-    access_token = accessTokenFileRead.read().rstrip()
-    accessTokenFileRead.close()
-
-    # make client
-    client = dropbox.client.DropboxClient(access_token)
-
-    # upload file
-    fileToUploadObject = open(fileToUpload, "rb")
-    response = client.put_file(fileToUpload, fileToUploadObject)
-    fileToUploadObject.close()
-
-
 try:
     while True:
         time.sleep(0.5)
@@ -85,7 +37,10 @@ try:
         GPIO.output(relay_pin, 1)
         camera.stop_recording()
         print("uploading to Dropbox")
-        dropboxUpload(filename)
+        p = subprocess.Popen([sys.executable, 'dropboxUploader.py', filename],
+                           stdout=subprocess.PIPE,
+                           stderr=subprocess.STDOUT)
+        # dropboxUploader.dropboxUpload(filename)
         print("uploaded to Dropbox")
 finally:
     camera.close()
