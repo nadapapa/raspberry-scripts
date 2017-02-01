80c0ffede0447a7d2a959954211da4417831d4ad
nadapapa <nadapapa@gmail.com>
2017-02-01 13:42:17 +0100

    add video conversion

--- motion.py
+++ motion.py
@@ -9,6 +9,8 @@ import sys
 import subprocess
 
 relay_pin = 17
+raw_video_extension = '.h264'
+converted_video_extension = '.mp4'
 
 GPIO.setmode(GPIO.BCM)
 
@@ -31,23 +33,30 @@ try:
         pir.wait_for_motion()
         print("motion")
         GPIO.output(relay_pin, 0)
-        filename = "/home/pi/videos/" + datetime.now().strftime("%Y-%m-%d_%H.%M.%S.h264")
+        now_time = datetime.now().strftime("%Y-%m-%d_%H.%M.%S")
+        filename = "/home/pi/videos/" + now_time
         camera.capture("/home/pi/motion.jpg")
         p = subprocess.Popen(["mpack", "-s", "motion detected", "/home/pi/motion.jpg", "nadapapa@gmail.com"],
                              stdout=subprocess.PIPE,
                              stderr=subprocess.STDOUT)
-        camera.start_recording(filename)
+        camera.start_recording(filename + raw_video_extension)
 
         pir.wait_for_no_motion()
         print("no motion")
         GPIO.output(relay_pin, 1)
         camera.stop_recording()
+
+        print("converting to mp4")
+        p = subprocess.Popen(['MP4Box', '-add', filename + raw_video_extension, filename + converted_video_extension],
+                             stdout=subprocess.PIPE,
+                             stderr=subprocess.STDOUT)
+        p.wait()
+
         print("uploading to Dropbox")
         log = open('/tmp/log2', 'w')
-        p = subprocess.Popen(['/home/pi/raspberry-scripts/dropboxUploader.py', filename],
+        p = subprocess.Popen(['/home/pi/raspberry-scripts/dropboxUploader.py', filename + converted_video_extension],
                            stdout=log,
                            stderr=subprocess.STDOUT)
-        print("uploaded to Dropbox")
 finally:
     camera.close()
     pir.close()
