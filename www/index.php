98e12129021fc0b8403ad8b050c891a75a2da44f
nadapapa <nadapapa@gmail.com>
2017-05-17 18:29:56 +0200

    add some style

--- www/index.php
+++ www/index.php
@@ -2,18 +2,32 @@
 <!-- wip -->
 <head>
     <title>home security system admin</title>
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
+    <style>
+      .btn {
+        font-size: 50px;
+      }
+    </style>
 </head>
 <body>
-    <form action="index.php" method="post">
-        <input type="hidden" name="on" value="true"><br>
-        <input type="submit" value="ON">
-    </form>
-    <br>
-    <form action="index.php" method="post">
-        <input type="hidden" name="off" value="true"><br>
-        <input type="submit" value="OFF">
-    </form>
+  <div class="row">
+    <div class="col-xs-12" style="height:30%;"></div>
+  </div>
+  <div class="row">
+    <div class="col-xs-12">
+      <form action="index.php" method="post">
+          <input type="hidden" name="on" value="true"><br>
+          <button type="submit" class="btn btn-primary btn-lg btn-block" value="ON">ON</button>
+      </form>
+      <br>
+      <form action="index.php" method="post">
+          <input type="hidden" name="off" value="true"><br>
+          <button type="submit" class="btn btn-danger btn-lg btn-block" value="OFF">OFF</button>
+      </form>
+    </div>
+  </div>
 </body>
+
 </html>
 
 <?php
